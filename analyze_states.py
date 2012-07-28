77e8973fcff2421a37378eb5ad3221aadd204695
Malcolm Sharpe <sharpe.malcolm@gmail.com>
2012-07-28 11:55:35 -0400

    More stats.

--- analyze_states.py
+++ analyze_states.py
@@ -10,7 +10,7 @@ NUM_PROP = 4
 
 # Modify these to see different stats.
 name = 'jsharpe'
-game_type = '3tau'
+game_type = 'i3tau'
 
 session = get_session()
 scores = (session.query(Score)
@@ -51,6 +51,9 @@ def get_spacing(cards, board):
 
   return (mxr-mnr+1) * (mxc-mnc+1)
 
+def get_diff_vector(cards):
+  return tuple([not is_same_in(cards,i) for i in range(NUM_PROP)])
+
 # Find times by number of differing properties.
 differ_stats = defaultdict(lambda: [])
 
@@ -59,6 +62,9 @@ posn_stats = defaultdict(lambda: [])
 
 # Find times by some property being the same across the 3 cards.
 same_stats = defaultdict(lambda: [])
+
+# Find times by which properties are different.
+diffvec_stats = defaultdict(lambda: [])
 
 print 'Processing scores...'
 S = 0
@@ -81,6 +87,8 @@ for score in scores:
       if is_same_in(cards, i):
         same_stats[i].append(delta)
 
+    diffvec_stats[get_diff_vector(cards)].append(delta)
+
 print 'All done!'
 print '(There were %d scores.)' % S
 print
@@ -88,32 +96,48 @@ print
 print 'Player: %s' % name
 print 'Game type: %s' % game_type
 print
+
+def pr_deltas(deltas):
+  print '  count: %d' % len(deltas)
+  # To get something meaningful from the mean, the data will need to be cleaned.
+  #print '  mean: %.3lf' % np.mean(deltas)
+  print '  25%%:    %6.3lf' % np.percentile(deltas, 25.)
+  print '  median: %6.3lf' % np.median(deltas)
+  print '  75%%:    %6.3lf' % np.percentile(deltas, 75.)
+  print
+
+def pr_hr():
+  print 60*'*'
+
+pr_hr()
 print '*** Number of differing properties'
 for x in sorted(differ_stats):
   deltas = differ_stats[x]
 
   print '# differing properties = %d' % x
-  print '  count: %d' % len(deltas)
-  # To get something meaningful from the mean, the data will need to be cleaned.
-  #print '  mean: %.3lf' % np.mean(deltas)
-  print '  median: %.3lf' % np.median(deltas)
-  print
+  pr_deltas(deltas)
 
+pr_hr()
 print '*** Area of card bounding box'
 for a in sorted(posn_stats):
   deltas = posn_stats[a]
 
   print 'Bounding box area = %d' % a
-  print '  count: %d' % len(deltas)
-  print '  median: %.3lf' % np.median(deltas)
-  print
+  pr_deltas(deltas)
 
+pr_hr()
 print '*** Property that is the same'
 PROP_NAMES = ['colour', 'number', 'fill', 'shape']
 for i in sorted(same_stats):
   deltas = same_stats[i]
 
   print 'Has same property = %s' % PROP_NAMES[i]
-  print '  count: %d' % len(deltas)
-  print '  median: %.3lf' % np.median(deltas)
-  print
+  pr_deltas(deltas)
+
+pr_hr()
+print '*** Different-property vector'
+for v in sorted(diffvec_stats, key=sum):
+  deltas = diffvec_stats[v]
+
+  print 'Different properties = %s' % ', '.join(PROP_NAMES[i] for i in range(NUM_PROP) if v[i])
+  pr_deltas(deltas)
