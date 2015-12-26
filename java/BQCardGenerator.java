473e36a53b38944a44bdef93469d60e88aba0410
Jeremy Sharpe <jeremy.adamson.sharpe@gmail.com>
2015-12-26 17:55:10 -0500

    Experimental card generation.

--- java/BQCardGenerator.java
+++ java/BQCardGenerator.java
@@ -3,6 +3,7 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.Shape;
+import java.awt.BasicStroke;
 import java.awt.TexturePaint;
 import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
@@ -13,6 +14,10 @@ import java.util.List;
 import javax.imageio.ImageIO;
 
 public class BQCardGenerator {
+  static final double SCALE = 0.15;
+  static final int WIDTH = (int) (825 * SCALE);
+  static final int HEIGHT = (int) (1125 * SCALE);
+
   public static void main(String[] args) {
     if (args.length != 1) {
       throw new IllegalArgumentException(
@@ -20,24 +25,24 @@ public class BQCardGenerator {
     }
 
     //colour, number, shape
-    int width = 825;
-    int height = 1125;
+    int width = (int) (825 * SCALE);
+    int height = (int) (1125 * SCALE);
     int i = 0;
     for (int colour = 0; colour < 4; colour++) {
       for (int number = 0; number < 4; number++) {
         for (int shape = 0; shape < 4; shape++) {
-          File imageFile = new File(args[0] + "_" + i + ".png");
+          File imageFile = new File(args[0] + "_" + i + "_small.png");
 
           BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
           Graphics2D graphics = bufferedImage.createGraphics();
           
-          graphics.setBackground(new Color(0xFFFFFF));
+          graphics.setBackground(new Color(0xEDEDED));
           graphics.clearRect(0, 0, width, height);
 
           graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-          int bonusLeft = 60;
-          int bonusTop = 81;
+          int bonusLeft = (int) (60 * SCALE);
+          int bonusTop = (int) (81 * SCALE);
           drawCard(graphics, -bonusLeft, -bonusTop, width + 2*bonusLeft, height+2*bonusTop, colour, number, shape);
 
           try {
@@ -55,6 +60,10 @@ public class BQCardGenerator {
   private static void drawCard(Graphics2D graphics, int xBase,
       int yBase, int width, int height, int colour, int number,
       int shape) {
+    // draw card
+    graphics.setColor(new Color(0xFFFFFF));
+    graphics.fillRoundRect(0, 0, WIDTH, HEIGHT, 20, 20);
+
     // get shape positions
     int[] xs;
     int[] ys;
@@ -91,17 +100,17 @@ public class BQCardGenerator {
     }
 
     List<Shape> shapes = new ArrayList<Shape>();
-    int number1 = 118;
-    int number2 = 236;
+    int half_shape_width = (int) (118 * SCALE);
+    int shape_width = (int) (236 * SCALE);
     for (int i = 0; i < number + 1; i++) {
       if (shape == 0) {
-        shapes.add(new Ellipse2D.Double(xs[i] - number1, ys[i] - number1, number2, number2));  
+        shapes.add(new Ellipse2D.Double(xs[i] - half_shape_width, ys[i] - half_shape_width, shape_width, shape_width));  
       } else if (shape == 1) {
-        shapes.add(new Rectangle(xs[i] - number1, ys[i] - number1, number2, number2));
+        shapes.add(new Rectangle(xs[i] - half_shape_width, ys[i] - half_shape_width, shape_width, shape_width));
       } else if (shape == 2) {
-        shapes.add(new Triangle(xs[i] - number1, ys[i] - number1, number2, number2).getShape());
+        shapes.add(new Triangle(xs[i] - half_shape_width, ys[i] - half_shape_width, shape_width, shape_width).getShape());
       } else {
-        shapes.add(new Diamond(xs[i] - number1, ys[i] - number1, number2, number2).getShape());
+        shapes.add(new Diamond(xs[i] - half_shape_width, ys[i] - half_shape_width, shape_width, shape_width).getShape());
       }
     }
 
--- java/cardbqcycle.sh
+++ java/cardbqcycle.sh
@@ -1,7 +1,7 @@
 # Run a complete cycle of physical bqtau card generation, previewing results.
 set -e
 javac *.java
-rm -rf cards
-mkdir cards
-java BQCardGenerator cards/bqcard
-open cards/bqcard_0.png
+rm -rf small_cards
+mkdir small_cards
+java BQCardGenerator small_cards/bqcard
+open small_cards/bqcard_0_small.png
