55044873c5655f3a27c2d7e11224d55853fad0d6
Jeremy Sharpe <jeremy.adamson.sharpe@gmail.com>
2026-08-22 11:32:28 -0400

    Enable classic cards for everyone.

--- static/tau.js
+++ static/tau.js
@@ -314,7 +314,7 @@ $(document).ready(function() {
 
   function getImgClass() {
     var colour_blind = $.cookie("colour_blind") === "true";
-    var classic_cards = $.cookie("classic_cards") === "true";
+    var classic_cards = $.cookie("classic_cards") !== "false";
     var new_projective_cards = true;
     var hanchul_222_projective_cards = $.cookie("hanchul_proj_222") === "true";
     var hanchul_quad_cards = $.cookie("hanchul_quad") === "true"
